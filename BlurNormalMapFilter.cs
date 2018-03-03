e168a7ad4eee59fc77e49e772cb195fc4eb7b33e
medvedya <modernup@mail.ru>
2018-03-04 01:31:43 +0300

    Init

---
+++ BlurNormalMapFilter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+namespace HeightMap2NormalMap
+{
+    [System.Serializable]
+    public struct BlurNormalMapFilter : IFilter
+    {
+        public GaussianBlurFilter preBlur;
+        public SobelNormalMapFilter normalMap;
+        public GaussianBlurFilter postBlur;
+
+        public void Apply(Texture source, RenderTexture destination)
+        {
+            int w = source.width, h = source.height;
+            var rd = new RenderTextureDescriptor(w, h)
+            {
+                sRGB = false
+            };
+            var blurRT = RenderTexture.GetTemporary(rd);
+            preBlur.Apply(source, blurRT);
+            var tmpNormalRT = RenderTexture.GetTemporary(rd);
+            normalMap.Apply(blurRT, tmpNormalRT);
+            RenderTexture.ReleaseTemporary(blurRT);
+            var blurRT2 = RenderTexture.GetTemporary(rd);
+            postBlur.Apply(tmpNormalRT, blurRT2);
+            RenderTexture.ReleaseTemporary(tmpNormalRT);
+            (new NormalizeNormalMapFilter()).Apply(blurRT2, destination);
+            RenderTexture.ReleaseTemporary(blurRT2);
+        }
+    }
+}
---
+++ Shaders/HeightMap.shader
@@ -0,0 +1,53 @@
+﻿Shader "Hidden/NMG/HeightMap"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_Factor("_Factor", FLOAT) = 1
+	}
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			sampler2D _MainTex;
+			float _Factor;
+			fixed4 frag (v2f i) : SV_Target
+			{
+				float4 col = tex2D(_MainTex, i.uv);
+				col.rgb *= col.a;
+				col.rgb *=_Factor;
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
